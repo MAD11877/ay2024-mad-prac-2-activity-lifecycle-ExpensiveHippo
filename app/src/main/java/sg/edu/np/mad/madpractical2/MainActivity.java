41ba39cdc574b6f94d3b67a01d188ec6e6fc067c
Ryan <s10261024@connect.np.edu.sg>
2024-04-24 13:06:14 +0800

    finish Practical 2 challenge 2

--- app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical2/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
